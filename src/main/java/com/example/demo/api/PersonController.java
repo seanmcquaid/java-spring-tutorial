8adf43fa2b8ea9b29f5eaa788cc2abca3fff0380
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-07 15:22:40 -0400

    added annotations to handle dependency injection

---
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -0,0 +1,21 @@
+package com.example.demo.api;
+
+import com.example.demo.model.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService){
+        this.personService = personService;
+    }
+
+    public void addPerson(Person person){
+        personService.addPerson(person);
+    }
+}
