acb717bc3bc97a0db64c608d4724e28181aae9d5
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-07 15:31:22 -0400

    set up first end point

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,12 +1,14 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
 public class Person {
     private final String name;
     private final UUID id;
 
-    public Person(UUID id, String name) {
+    public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
         this.name = name;
         this.id = id;
     }
