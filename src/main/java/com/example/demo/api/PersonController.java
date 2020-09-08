200925b2afd499d633b70300192fbbd9b4270a15
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-07 20:49:14 -0400

    making progress on setting up Postgres

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -4,7 +4,6 @@ import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
